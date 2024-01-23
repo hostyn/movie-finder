dc91d843a025c33fc65dd74df30e37c1f532707c
Hostyn <ruben@martinezhostyn.com>
2024-01-23 23:37:54 +0100

    feat: :lipstick: Add border to Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -46,16 +46,18 @@ export default function Input({
       onBlur={handleBlur}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="relative flex overflow-hidden w-[32rem] cursor-text items-center gap-4 text-slate-300 rounded-full px-6 py-4 bg-[#1d1728] focus-within:outline-none focus-within:ring-2 focus-within:ring-slate-300 focus-within:ring-offset-2 focus-within:ring-offset-slate-950 transition duration-300"
+      className="relative flex overflow-hidden w-[32rem] cursor-text items-center gap-4 text-slate-300 rounded-full px-6 py-4 bg-[#1d1728] focus-within:outline-none focus-within:ring-2 focus-within:ring-[#8678F9] focus-within:ring-offset-2 focus-within:ring-offset-slate-950 transition duration-300"
       style={{
         background: isFocused ? "#2d243e" : "#1d1728",
       }}
     >
       <div
-        className="absolute -inset-px pointer-events-none transition duration-300"
+        className="absolute inset-px pointer-events-none transition duration-300 rounded-full"
         style={{
           opacity: isFocused ? 0 : opacity,
-          background: `radial-gradient(100px 80px at ${position.x}px ${position.y}px, #2d243e, transparent 100%)`,
+          background: `radial-gradient(100px 800px at ${position.x}px ${position.y}px, #2d243e 45%, transparent 100%)`,
+          WebkitMaskImage: `radial-gradient(80px 800px at ${position.x}px ${position.y}px, black 45%, transparent)`,
+          border: "1px solid #8678F9",
         }}
       />
       <SearchIcon className="z-10 text-[#565c7a]" />
