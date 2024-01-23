566c2644a1f6b0baccc2311671bd0866eefcbd7f
Hostyn <ruben@martinezhostyn.com>
2024-01-23 19:30:43 +0100

    fix: :bug: Move background to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-950 relative z-[-10]">
-      <div className="absolute z-[-5] bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-6xl">Find a movie</h1>
     </main>
   );
