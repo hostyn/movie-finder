01f86595234489ec9a8b887bc84190826f96ae29
Hostyn <ruben@martinezhostyn.com>
2024-01-25 12:57:51 +0100

    feat: :zap: Add react-query prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-"use client";
-
-import Input from "@/components/Input";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import getQueryClient from "./getQueryClient";
+import { algolia } from "@/services/algolia";
+import { Movie } from "@/types/algolia";
 import Search from "@/components/Search";
-import { useState } from "react";
 
-export default function Home() {
-  const [query, setQuery] = useState("");
+export default async function Home() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["search", ""],
+    queryFn: () => algolia.search<Movie>(""),
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-8">
-      <h1 className="text-6xl">Find a movie</h1>
-      <Input
-        placeholder="The Machinist"
-        value={query}
-        onChange={({ target }) => setQuery(target.value)}
-      />
-      <Search query={query} />
-    </main>
+    <HydrationBoundary state={dehydratedState}>
+      <main className="flex min-h-screen flex-col items-center p-24 gap-8">
+        <h1 className="text-6xl">Find a movie</h1>
+        <Search />
+      </main>
+    </HydrationBoundary>
   );
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,34 @@
+"use client";
+
 import { algolia } from "@/services/algolia";
 import { Movie } from "@/types/algolia";
 import MovieCard from "./MovieCard";
 import { useQuery } from "@tanstack/react-query";
+import Input from "./Input";
+import { useState } from "react";
 
-export default function Search({ query }: { query: string }) {
+export default function Search() {
+  const [query, setQuery] = useState("");
   const { data } = useQuery({
     queryKey: ["search", query],
     queryFn: () => algolia.search<Movie>(query),
   });
 
+  console.log(data);
+
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {data?.hits.map((hit) => (
-        <MovieCard key={hit.objectID} movie={hit} />
-      ))}
+    <div className="flex flex-col items-center gap-8">
+      <Input
+        placeholder="The Machinist"
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
+      />
+
+      <div className="grid grid-cols-4 gap-4">
+        {data?.hits.map((hit) => (
+          <MovieCard key={hit.objectID} movie={hit} />
+        ))}
+      </div>
     </div>
   );
 }
