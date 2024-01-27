e1fc8b1a0963f7361e7141b0a4d61ac1eb27ff0c
Hostyn <ruben@martinezhostyn.com>
2024-01-27 12:57:52 +0100

    fix: :iphone: Adjust background height to 100vh to prevent white strip on mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <div className="fixed left-0 top-0 -z-10 h-full w-full">
+        <div className="fixed left-0 top-0 -z-10 h-[100lvh] w-full">
           <div className="relative h-full w-full bg-slate-950">
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
           </div>
