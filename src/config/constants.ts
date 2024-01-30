a2904f7b75417ba6786bf826adc7e18ac48660cf
Hostyn <ruben@martinezhostyn.com>
2024-01-30 20:13:12 +0100

    feat: :wrench: Add constants

---
+++ src/config/constants.ts
@@ -0,0 +1,35 @@
+import { Genre } from "@/types/algolia";
+
+export const MAX_YEAR = 2031;
+export const MIN_YEAR = 1894;
+
+export const GENRES: Genre[] = [
+  "Action",
+  "Adult",
+  "Adventure",
+  "Animation",
+  "Biography",
+  "Comedy",
+  "Crime",
+  "Documentary",
+  "Drama",
+  "Family",
+  "Fantasy",
+  "Film-Noir",
+  "Game-Show",
+  "History",
+  "Horror",
+  "Music",
+  "Musical",
+  "Mystery",
+  "News",
+  "Reality-TV",
+  "Romance",
+  "Sci-Fi",
+  "Short",
+  "Sport",
+  "Talk-Show",
+  "Thriller",
+  "War",
+  "Western",
+];
--- src/types/algolia.d.ts
+++ src/types/algolia.d.ts
@@ -3,9 +3,39 @@ export interface Movie {
   primaryTitle: string;
   originalTitle: string;
   year: number;
-  genres: string[] | null;
+  genres: Genre[];
   averageRating: number | null;
   numVotes: number;
   poster: string | null;
   runtimeMinutes: number | null;
 }
+
+export type Genre =
+  | "Action"
+  | "Adult"
+  | "Adventure"
+  | "Animation"
+  | "Biography"
+  | "Comedy"
+  | "Crime"
+  | "Documentary"
+  | "Drama"
+  | "Family"
+  | "Fantasy"
+  | "Film-Noir"
+  | "Game-Show"
+  | "History"
+  | "Horror"
+  | "Music"
+  | "Musical"
+  | "Mystery"
+  | "News"
+  | "Reality-TV"
+  | "Romance"
+  | "Sci-Fi"
+  | "Short"
+  | "Sport"
+  | "Talk-Show"
+  | "Thriller"
+  | "War"
+  | "Western";
